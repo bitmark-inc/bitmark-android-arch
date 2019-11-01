b0618c2dc6f8f17adfc0e77de0d98a596bb13f53
Hieu Pham <hieupham@bitmark.com>
2019-11-01 13:26:03 +0700

    add main screen for launching app

---
+++ app/src/main/java/com/bitmark/standardmobile/feature/main/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.bitmark.standardmobile.feature.main
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.bitmark.standardmobile.R
+
+
+/**
+ * @author Hieu Pham
+ * @since 2019-10-31
+ * Email: hieupham@bitmark.com
+ * Copyright © 2019 Bitmark. All rights reserved.
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
